0c2dd1fa7a1f0d70d0fc6513c7e244cdf8d4baff
Leonel Garza Loperena <leonel.garza@prolecge.com>
2022-05-18 09:19:42 -0500

    add style and functions in javascript

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 const userChoiceDisplay = document.createElement('h1')
 const computerChoiceDisplay = document.createElement('h1')
+const userChoiceImg = document.createElement('img')
+const computerChoiceImg = document.createElement('img')
+const match = document.createElement('div')
+const vs = document.createElement('img')
 const resultDisplay = document.createElement('h1')
 
 const gameGrid = document.querySelector('#game')
 
-gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
-
 const choices = ['rock', 'papper', 'scissors']
+
+vs.src = 'Street_Fighter_VS_logo.png'
+vs.classList.add('imgMatch')
+userChoiceImg.classList.add('imgMatch')
+computerChoiceImg.classList.add('imgMatch')
+match.classList.add('match')
 
 handleClick = (e) => {
     const userChoice = e.target.innerHTML
     userChoiceDisplay.innerHTML = 'You select:   ' + userChoice
+    userChoiceImg.src = userChoice + '.jpg'
     const randomChoice = choices[Math.floor(Math.random()*choices.length)]
     computerChoiceDisplay.innerHTML = 'cpu select: ' + randomChoice
+    computerChoiceImg.src = randomChoice + '.jpg'
     getResult(userChoice, randomChoice)
+    match.append(userChoiceImg, vs, computerChoiceImg)
 }
 
 choices.forEach(choice => {
     const button = document.createElement('button')
+    button.classList.add('gamebutton')
     button.innerHTML = choice
     button.addEventListener('click', handleClick)
 
     gameGrid.appendChild(button)
 })
+
+gameGrid.append(match, userChoiceDisplay, computerChoiceDisplay, resultDisplay)
 
 const getResult = (userChoice, randomChoice) => {
     switch(userChoice + randomChoice){
@@ -31,7 +45,7 @@ const getResult = (userChoice, randomChoice) => {
         case 'scissorspapper':
             resultDisplay.innerHTML = 'YOU WIN!'
             break
-        case 'scissorsrocks':
+        case 'scissorsrock':
         case 'rockpapper':
         case 'papperscissors':
             resultDisplay.innerHTML = 'YOU LOSE!'
